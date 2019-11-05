44fd4a679047ecd5fcdc4141369cbf0b5a5b9b3b
Noah Gibson <135noahg@gmail.com>
2019-11-04 23:09:02 -0500

    fixed issue with prompt text not showing properly

--- app/src/main/java/com/example/cse5236app/ui/CameraPrompt.java
+++ app/src/main/java/com/example/cse5236app/ui/CameraPrompt.java
@@ -10,26 +10,35 @@ import com.example.cse5236app.R;
 
 public class CameraPrompt extends View {
 
-    private Paint paint = new Paint();
+    private Paint rectPaint = new Paint();
+    private Paint promptPaint = new Paint();
 
     CameraPrompt(Context context) {
         super(context);
+
+        rectPaint.setStyle(Paint.Style.STROKE);
+        rectPaint.setColor(Color.WHITE);
+        rectPaint.setStrokeWidth(30);
+
+        promptPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        promptPaint.setColor(Color.WHITE);
+        promptPaint.setStrokeWidth(3);
+        promptPaint.setTextSize(75.0f);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.WHITE);
-        paint.setStrokeWidth(30);
-
         int x0 = getWidth() / 2;
         int y0 = getHeight() / 2;
         int dx = getWidth() / 4;
         int dy = getHeight() / 4;
 
-        canvas.drawText(getResources().getString(R.string.text_scanner_prompt), x0 - dx, y0 - dy - 50, paint);
-        canvas.drawRect(x0 - dx, y0 - dy, x0 + dx, y0 + dy, paint);
+        String prompt = getResources().getString(R.string.text_scanner_prompt);
+        float promptWidth = promptPaint.measureText(prompt);
+
+        canvas.drawText(prompt, x0 - promptWidth / 2, y0 - dy - 100, promptPaint);
+        canvas.drawRect(x0 - dx, y0 - dy, x0 + dx, y0 + dy, rectPaint);
     }
 }
