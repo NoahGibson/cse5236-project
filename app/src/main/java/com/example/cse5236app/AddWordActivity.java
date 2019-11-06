0ba4df29c1b1744294d44a117ca687ebcbe4b8c3
khloekok <khloekok@gmail.com>
2019-11-06 00:41:37 -0500

    API call to get definition finished

--- app/src/main/java/com/example/cse5236app/AddWordActivity.java
+++ app/src/main/java/com/example/cse5236app/AddWordActivity.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.net.URL;
 
 public class AddWordActivity extends AppCompatActivity {
 
@@ -16,16 +21,37 @@ public class AddWordActivity extends AppCompatActivity {
     public static final String EXTRA_DEF = "com.example.cse5236app.EXTRA_DEF";
 
     private EditText editTextWord;
-    private EditText editTextDefinition;
+    private TextView textDefinition;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_word);
 
         editTextWord = findViewById(R.id.edit_text_word);
-        editTextDefinition = findViewById(R.id.edit_text_definition);
+        textDefinition = findViewById(R.id.edit_text_definition);
+
+
+        Button button_definition = (Button) findViewById(R.id.button_get_definition);
+        button_definition.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                requestAPIButtonClick(v);
+            }
+        });
     }
+
+
+    /**
+     * passes url
+     */
+    public void requestAPIButtonClick(View v){
+        DictionaryRequest dictionaryRequest = new DictionaryRequest(this, textDefinition);
+        dictionaryRequest.execute(editTextWord.getText().toString());
+    }
+
 
     /**
      * using our options menu created
@@ -58,7 +84,7 @@ public class AddWordActivity extends AppCompatActivity {
      */
     public void saveWord(){
         String word_name = editTextWord.getText().toString();
-        String definition = editTextDefinition.getText().toString();
+        String definition = textDefinition.getText().toString();
 
         if(word_name.trim().isEmpty() || definition.trim().isEmpty()){
             Toast.makeText(this, "Please add word or definition", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/com/example/cse5236app/DictionaryRequest.java
@@ -0,0 +1,94 @@
+package com.example.cse5236app;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class DictionaryRequest extends AsyncTask<String,Integer,String>{
+
+    Context context;
+    TextView t1;
+    EditText e1;
+
+    DictionaryRequest(Context context, TextView t1){
+        this.context = context;
+        this.t1 = t1;
+    }
+
+
+    @Override
+    protected String doInBackground(String...params) {
+
+        String word = params[0];
+        String myUrl;
+        final String app_id = "d06e0e07";
+        final String app_key = "2a1fa1e4702a4ab28f11be906c5c8eec";
+
+        /**
+         * creates the url
+         */
+            final String language = "en-gb";
+            final String word_id = word.toLowerCase();
+            myUrl = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id;
+
+        try {
+            URL url = new URL(myUrl);
+            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+            urlConnection.setRequestProperty("Accept","application/json");
+            urlConnection.setRequestProperty("app_id",app_id);
+            urlConnection.setRequestProperty("app_key",app_key);
+
+            // read the output from the server
+            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line + "\n");
+            }
+
+            return stringBuilder.toString();
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        String def;
+        super.onPostExecute(result);
+
+        try{
+            JSONObject js = new JSONObject(result);
+            JSONArray json_result = js.getJSONArray("results")
+                    .getJSONObject(0)
+                    .getJSONArray("lexicalEntries")
+                    .getJSONObject(0)
+                    .getJSONArray("entries")
+                    .getJSONObject(0)
+                    .getJSONArray("senses")
+                    .getJSONObject(0)
+                    .getJSONArray("definitions");
+
+            def = json_result.getString(0);
+            t1.setText(def);
+
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
